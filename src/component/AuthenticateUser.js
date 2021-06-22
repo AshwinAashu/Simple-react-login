1b936236f42c8bf16519217f37f323960257d118
AshwinAashu <80351416+AshwinAashu@users.noreply.github.com>
2021-06-22 19:04:35 +0530

    login page without auth

---
+++ src/component/AuthenticateUser.js
@@ -0,0 +1,19 @@
+//get the local submitted username and password 
+//call the api to retrieve the data for the username from the backend 
+//compare the username and password
+//return the acess to landing page 
+
+// import React from 'react';
+// import axios from 'axios';
+
+// function AuthenticateUser(user, pass) {
+//     try{
+//         axios.get('localhost:9090/api/credentials/get?username={this.user}');
+
+//     }catch(e){
+//         return false;
+//     }
+    
+
+
+// }
---
+++ src/component/Login.js
@@ -0,0 +1,74 @@
+import React, { Component} from 'react';
+import  axios from 'axios';
+import { Route, Redirect } from 'react-router';
+// import LandingPage from './LandingPage';
+
+class Login extends Component {
+    constructor(){
+        super();
+        this.state = {
+            email: "",
+            pasw : "",
+            loggedIn :  false,
+        }
+        // this.setState =  this.setState.bind(this);
+    }
+
+    emailHandler=(e)=>{
+        e.preventDefault();
+        this.setState({email : e.target.value});
+    }
+
+    pswHandler = (e) =>{
+        e.preventDefault();
+        this.setState({pasw : e.target.value});
+    }
+    
+       
+    
+
+    submitHandler = (e)=> {
+        e.preventDefault();
+        //should call the authenticate function 
+        //using axios  to see if we can get username directly
+        const LOCAL_DATA= {
+            user : this.state.email,
+            password: this.state.pasw, 
+        }
+        
+        axios.get(`http://localhost:9090/api/credential/get?username=${LOCAL_DATA.user}`)
+        .then(res => {
+            if(LOCAL_DATA.password === res.data.password){
+                //set loggedin to true
+                this.setState({loggedIn: true});
+               
+            }
+            else alert("Email or password incorrect");
+        
+        })
+        .catch((e)=> console.log(e));
+
+        <Route>
+            {this.state.loggedIn ? <Redirect to="/profile"  /> : <Login/>}
+        </Route>
+    
+    }
+
+
+    render(){
+        
+        
+        return(
+            <div>
+                <h1>Login to continue</h1>
+                <form onSubmit = {this.submitHandler}>
+                    <input onChange = { this.emailHandler } type="email" value={this.state.email} className="emailform" placeholder="abc@xyz.com"/>  <br/>
+                    <input onChange = {this.pswHandler} type="password" name="password" className="emailform" value={this.state.pasw} placeholder="Password"/> <br/>
+                    <input type="submit" className="submitbutton" value="LogIn"/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Login
